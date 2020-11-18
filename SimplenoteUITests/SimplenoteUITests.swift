0473e8bd975c3975b1c43912b6998896970c22ce
pachlava <73365754+pachlava@users.noreply.github.com>
2020-11-19 00:27:41 +0200

    Moved clearAndEnterText extension for XCUIElement to a separate file

--- SimplenoteUITests/SimplenoteUITests.swift
+++ SimplenoteUITests/SimplenoteUITests.swift
@@ -377,23 +377,3 @@ class Assert {
         XCTAssertTrue(app.buttons[uidButton_LogIn].waitForExistence(timeout: minLoadTimeout), uidButton_LogIn + buttonNotFound)
     }
 }
-
-extension XCUIElement {
-    /**
-     Removes any current text in the field before typing in the new value
-     - Parameter text: the text to enter into the field
-     */
-    func clearAndEnterText(text: String) {
-        guard let stringValue = self.value as? String else {
-            XCTFail("Tried to clear and enter text into a non string value")
-            return
-        }
-
-        self.tap()
-
-        let deleteString = String(repeating: XCUIKeyboardKey.delete.rawValue, count: stringValue.count)
-
-        self.typeText(deleteString)
-        self.typeText(text)
-    }
-}
